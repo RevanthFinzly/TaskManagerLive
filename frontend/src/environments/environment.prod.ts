9ce70815d0715fa3501086bec1f77fb5299aebd8
RevanthFinzly <revanthroj.venkatachalam@finzly.com>
2025-09-06 06:11:01 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crud-app-backend.onrender.com/api'
-  // Note: Replace 'crud-app-backend' with your actual Render application name
+  apiUrl: 'https://taskmanagerlive.onrender.com/api'
 };
